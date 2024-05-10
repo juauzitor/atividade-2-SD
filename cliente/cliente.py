4da3af3e7ee0178c5ea4c644f0fcd42eba2f3572
João Vitor Mendes Pinto dos Santos <joao.v.m.p.santos@ieee.org>
2024-05-09 23:58:39 -0300

    feat: Show the ranking and improvemnt in output in cli

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -5,21 +5,37 @@ def main():
     server_url = 'http://localhost:8000'
     # Criando o cliente XML-RPC
     server = xmlrpc.client.ServerProxy(server_url)
-
-    try:
-        questions = server.QuizHandler.getQuestions()
-        respostas = []
-        for question in questions:
-            print("Pergunta:", question['pergunta'])
-            for alternativas in question['respostas']:
-                print("Alternativa: " + str(alternativas['alternativa']) + " " + alternativas['descricao'])
-            respostas.append(int(input("Sua resposta: ")))
-
-        acertos = server.QuizHandler.acertos(respostas)
-        print(acertos)
-    except Exception as e:
-        print("Erro ao receber questões:", e)
-
+    opcao = int(input("Digite 1 se deseja jogar ou 2 se deseja ver o ranking: "))
+    if opcao == 1:
+        try:
+            print("---------------------")
+            print()
+            nome = input("Qual seu nome: ")
+            print("---------------------")
+            print()
+            questions = server.QuizHandler.getQuestions()
+            respostas = []
+            for question in questions:
+                print("Pergunta:", question['pergunta'])
+                for alternativas in question['respostas']:
+                    print("Alternativa: " + str(alternativas['alternativa']) + " " + alternativas['descricao'])
+                respostas.append(int(input("Sua resposta: ")))
+                print()
+            acertos = server.QuizHandler.acertos(nome, respostas)
+            print(acertos)
+        except Exception as e:
+            print("Erro ao receber questões:", e)
+    else:
+        try:
+            print("---------------------")
+            print()
+            ranking = server.QuizHandler.getRanking()
+            idx = 1
+            for jogador in ranking:
+                print("Lugar: "+ str(idx) +"° |" + "jogador: " + jogador['nome'] + "| acertou: " + str(jogador['acertos']) + " Questões")
+                idx +=1
+        except Exception as e:
+            print("Erro ao receber ranking", e)
 
 if __name__ == "__main__":
     main()
